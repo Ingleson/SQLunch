10b8c417cedd936e23cfcb143342f3589a2becfb
Ingleson <jovemtksstranger@gmail.com>
2022-10-09 17:07:12 -0300

    crud, selects e tables

--- crud.sql
+++ crud.sql
@@ -5,30 +5,72 @@
 
 -- 1)
 
-
+    INSERT INTO clientes 
+      (nome, lealdade)
+    VALUES 
+      ('Georgia', 0);
 
 -- 2)
 
-
+    INSERT INTO pedidos 
+      (status, cliente_id)
+    VALUES 
+      ('Recebido', 6);
 
 -- 3)
 
-
+    INSERT INTO produtos_pedidos
+      (pedido_id, produto_id)
+    VALUES 
+      (6, 1),
+      (6, 2),
+      (6, 6),
+      (6, 8),
+      (6, 8);
 
 -- Leitura
 
 -- 1)
 
-
+    SELECT 
+      cl.id,
+      cl.nome,
+      cl.lealdade,
+      pe.id,
+      pe.status,
+      pe.cliente_id,
+      pr.id,
+      pr.nome,
+      pr.tipo,
+      pr.preco,
+      pr.pts_de_lealdade 
+    FROM 
+      clientes cl
+    JOIN
+      pedidos pe ON cl.id = pe.cliente_id
+    JOIN 
+      produtos_pedidos pp ON pe.id = pp.pedido_id
+    JOIN
+      produtos pr ON pp.produto_id = pr.id
+    WHERE 
+      cl.nome ILIKE '%georgia%';
 
 -- Atualização
 
 -- 1)
 
-
+    UPDATE 
+      clientes 
+    SET
+      lealdade = 48
+    WHERE 
+      nome = 'Georgia';
 
 -- Deleção
 
 -- 1)
 
-
+    DELETE FROM 
+      clientes
+    WHERE 
+      nome = 'Marcelo';
--- selects.sql
+++ selects.sql
@@ -3,10 +3,73 @@
 
 -- 1)
 
+    SELECT 
+      pe.id,
+      pe.status,
+      pe.cliente_id,
+      pr.id,
+      pr.nome,
+      pr.tipo,
+      pr.preco,
+      pr.pts_de_lealdade
+    FROM 
+      pedidos pe
+    JOIN
+      produtos_pedidos pp ON pe.id = pp.pedido_id 
+    JOIN
+      produtos pr ON pp.produto_id = pr.id;
+
 -- 2)
+
+    SELECT 
+      pe.id 
+    FROM 
+      pedidos pe
+    JOIN
+      produtos_pedidos pp ON pe.id = pp.pedido_id
+    JOIN
+      produtos pr ON pp.produto_id = pr.id
+    WHERE 
+      pr.nome ILIKE '%fritas%';
 
 -- 3)
 
+    SELECT 
+      cl.nome AS gostam_de_fritas
+    FROM 
+      clientes cl
+    JOIN 
+      pedidos pe ON cl.id = pe.cliente_id
+    JOIN
+      produtos_pedidos pp ON pe.id = pp.pedido_id
+    JOIN 
+      produtos pr ON pp.produto_id = pr.id
+    WHERE
+      pr.nome ILIKE '%fritas%';
+
 -- 4)
 
+    SELECT
+      sum(pr.preco) AS sum
+    FROM 
+      clientes cl
+    JOIN 
+      pedidos pe ON cl.id = pe.cliente_id
+    JOIN
+      produtos_pedidos pp ON pe.id = pp.pedido_id
+    JOIN 
+      produtos pr ON pp.produto_id = pr.id
+    WHERE
+      cl.nome ILIKE '%laura%';
+
 -- 5)
+
+    SELECT 	
+      nome,
+      COUNT(nome)
+    FROM 
+      produtos pr
+    JOIN
+      produtos_pedidos pp ON pr.id = pp.produto_id
+    GROUP BY 
+      nome;
